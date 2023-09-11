8a0455fabdcabdaeca44d5fa397f2994bf3b375a
Ali Hamdi Ali Fadel <AliOsm1997@gmail.com>
2023-09-11 12:45:29 +0000

    Implement dotenvs and model reader using optimum

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # baheth_ss
 
 الشيفرة البرمجية لدعم البحث بالمعنى على منصة باحث
+
+## لتشغيل الخادم المحلي
+
+```
+uvicorn baheth_ss.main:app --reload --env-file .env
+```
--- baheth_ss/main.py
+++ baheth_ss/main.py
@@ -1,16 +1,36 @@
-from fastapi import FastAPI
+import os
 
+import huggingface_hub
+
+from fastapi import FastAPI, status
+from fastapi.responses import JSONResponse
+from optimum.pipelines import pipeline
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    hf_access_token: str
+    hf_model_id: str
+
+
+settings = Settings()
+
+huggingface_hub.login(token=settings.hf_access_token)
 
 app = FastAPI()
+embedder = pipeline(task='feature-extraction', model=settings.hf_model_id, accelerator='ort')
 
 
 @app.post('/hadiths/semantic_search')
-def hadith_semantic_search(query: str) -> list[int]:
-    return []
+def hadiths_semantic_search(query: str) -> JSONResponse:
+    if embedder is None:
+        return JSONResponse(status_code=status.HTTP_503_SERVICE_UNAVAILABLE, content=[])
+
+    return JSONResponse(status_code=status.HTTP_200_OK, content=[])
 
 
 @app.post('/hadiths/count')
-def hadith_count() -> int:
+def hadiths_count() -> int:
     return 0
 
 
