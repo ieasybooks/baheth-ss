90cb762d0bfb44f331aa2ae438aa4bab0976268a
Ali Hamdi Ali Fadel <AliOsm1997@gmail.com>
2023-12-06 14:00:18 +0300

    Fix formatting issues

--- src/baheth_ss/responses/embed_response.py
+++ src/baheth_ss/responses/embed_response.py
@@ -1,4 +1,5 @@
 from pydantic import BaseModel
 
+
 class EmbedResponse(BaseModel):
     embeddings: list[list[float]]
--- src/baheth_ss/routers/embed.py
+++ src/baheth_ss/routers/embed.py
@@ -5,7 +5,9 @@ from fastapi import APIRouter, Request
 from src.baheth_ss.requests.embed_request import EmbedRequest
 from src.baheth_ss.responses.embed_response import EmbedResponse
 
+
 router = APIRouter(prefix='/embed', tags=['embed'])
+
 
 @router.post('', response_model=EmbedResponse)
 def embed(request: Request, embed_request: EmbedRequest) -> EmbedResponse:
