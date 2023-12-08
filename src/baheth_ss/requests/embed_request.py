424e4b9309fb4503481d2743d1449687b2030ff6
Ali Hamdi Ali Fadel <AliOsm1997@gmail.com>
2023-12-09 00:16:22 +0300

    Change queries to texts, Add type

--- src/baheth_ss/requests/embed_request.py
+++ src/baheth_ss/requests/embed_request.py
@@ -2,14 +2,12 @@ from pydantic import BaseModel, validator
 
 
 class EmbedRequest(BaseModel):
-    queries: str | list[str]
+    texts: str | list[str]
+    type: str
 
-    @validator('queries')
-    def validate_queries(cls, queries: str | list[str]) -> list[str]:
-        if isinstance(queries, str):
-            queries = [queries]
+    @validator('texts')
+    def validate_texts(cls, texts: str | list[str]) -> list[str]:
+        if isinstance(texts, str):
+            texts = [texts]
 
-        if len(queries) > 50 or max([len(query.split()) for query in queries]) > 25:
-            raise ValueError('Too many queries or too long query.')
-
-        return list(queries)
+        return list(texts)
--- src/baheth_ss/routers/embed.py
+++ src/baheth_ss/routers/embed.py
@@ -11,6 +11,6 @@ router = APIRouter(prefix='/embed', tags=['embed'])
 
 @router.post('', response_model=EmbedResponse)
 def embed(request: Request, embed_request: EmbedRequest) -> EmbedResponse:
-    queries = [f'query: {query}' for query in embed_request.queries]
+    texts = [f'{embed_request.type}: {text}' for text in embed_request.texts]
 
-    return EmbedResponse(embeddings=torch.stack((request.app.embedder(queries))).squeeze(1))
+    return EmbedResponse(embeddings=torch.stack((request.app.embedder(texts))).squeeze(1))
